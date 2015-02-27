f2fa89587d99dc37d972f9ff0b5c21f509c2e4ad
Hadrien Lanneau <hadrien@lanneau.me>
2015-02-27 09:43:18 +0100

    Welcome to the hatch

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import {Router} from 'aurelia-router';
+
+export class App {
+  static inject() { return [Router]; }
+  constructor(router) {
+    this.router = router;
+    this.router.configure(config => {
+      config.title = 'The Hatch';
+      config.map([
+        {
+          route: ['','welcome'],
+          moduleId: 'welcome/welcome'
+        },
+        {
+          route: 'users',
+          moduleId: 'users/users',
+          nav: true,
+          title: 'Users'
+        },
+        {
+          route: 'accounts',
+          moduleId: 'accounts/accounts',
+          nav: true,
+          title:'Accounts'
+        },
+        {
+          route: 'channels',
+          moduleId: 'channels/channels',
+          nav: true,
+          title: 'Channels'
+        }
+      ]);
+    });
+  }
+}
