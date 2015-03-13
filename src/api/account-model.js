db44cc696615f425f1adde2089ebf571cf48c03a
Hadrien Lanneau <hadrien@lanneau.me>
2015-03-13 10:18:24 +0100

    Generic model

--- src/api/account-model.js
+++ src/api/account-model.js
@@ -1,37 +1,52 @@
-export default class AccountModel {
+import Model from './model';
+
+export default class AccountModel extends Model {
   static properties () { return {
     id: null,
     name: null,
-    subdomain: null,
     createdAt: {
-      from: 'create_at',
+      from: 'created_at',
       type: Date
     },
     usersCount: {
-      from: 'users_count'
+      from: 'users_count',
+      type: Number
     },
     channelsCount: {
-      from: 'channels_count'
+      from: 'channels_count',
+      type: Number
+    },
+    lifetimeMessages: {
+      from: 'lifetime_messages',
+      type: Number
+    },
+    thirtyDaysMessages: {
+      from: 'thirty_days_messages',
+      type: Number
+    },
+    sevenDaysMessages: {
+      from: 'seven_days_messages',
+      type: Number
+    },
+    thirtyDaysEstimate: {
+      from: 'thirty_days_estimate',
+      type: Number
+    },
+    averageMessagesPerMonth: {
+      from: 'average_messages_per_month',
+      type: Number
+    },
+    inboxCount: {
+      from: 'inbox_count',
+      type: Number
+    },
+    taskCategoriesCount: {
+      from: 'task_categories_count',
+      type: Number
+    },
+    rulesCount: {
+      from: 'rules_count',
+      type: Number
     }
   }; }
-
-  constructor (data = {}) {
-    this.data = data;
-
-    var properties = AccountModel.properties();
-    for (var property in properties) {
-      this.setProperty(property);
-    }
-  }
-
-  setProperty (property) {
-    var properties = AccountModel.properties(),
-      definition = properties[property] || {},
-      propertyName = definition.from || property;
-
-    Object.defineProperty(this, property, {
-      get: () => this.data[propertyName],
-      set: (v) => this.data[propertyName] = v
-    });
-  }
 }
---
+++ src/api/model.js
@@ -0,0 +1,34 @@
+export default class Model {
+  static properties () { return {}; }
+
+  constructor (data = {}) {
+    this.data = data;
+
+    var properties = this.constructor.properties();
+    for (var property in properties) {
+      this.setProperty(property);
+    }
+  }
+
+  setProperty (property) {
+    var properties = this.constructor.properties(),
+      definition = properties[property] || {},
+      propertyName = definition.from || property;
+
+    Object.defineProperty(this, property, {
+      get: () => {
+        switch (definition.type) {
+          case Date:
+            return new Date(this.data[propertyName]);
+          case Number:
+            return +this.data[propertyName];
+          case String:
+            return `${this.data[propertyName]}`;
+          default:
+            return this.data[propertyName];
+        }
+      },
+      set: (v) => this.data[propertyName] = v
+    });
+  }
+}
