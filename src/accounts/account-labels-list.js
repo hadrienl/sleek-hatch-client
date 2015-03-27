b7aa1a76b70a2014ffeb836ebdbe7fb3acd6cdc9
Hadrien Lanneau <hadrien@lanneau.me>
2015-03-27 20:08:21 +0100

    Display labels

--- src/accounts/account-labels-list.js
+++ src/accounts/account-labels-list.js
@@ -1,1 +1,15 @@
-export class AccountLabelsList {}
+import {EventAggregator} from 'aurelia-event-aggregator';
+
+export class AccountLabelsList {
+  static inject () { return [EventAggregator]; }
+  constructor (EventAggregator) {
+    this.eventAggregator = EventAggregator;
+    this.eventAggregator.subscribe('account', account => this.setAccount(account));
+    this.labels = [];
+  }
+
+  setAccount (account) {
+    this.account = account;
+    this.labels = this.account.labels;
+  }
+}
